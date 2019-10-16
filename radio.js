00a06c745795423d0bdc88c99ba036d9b61ae1d3
孙杨博 <1412sybbob@163.com>
2019-10-16 14:16:52 +0800

    油猴插件：中国之声收听
    本以为需要解析mpegurl，ws，之类的。没想到audio插件就能满足。而且IE/EDGE可以直接解析m3u8。
    将来如果整合多电台|| 整合到chrome，再弄

---
+++ radio.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         广播挂件
+// @namespace    https://greasyfork.org/users/385498
+// @version      0.1
+// @description  直引到中国之声
+// @author       syb | 7P_LonG
+// @include *
+// @grant        none
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    const tipid = 'radio-text';//提示框id
+    const wintip = `<div id="radio-wrapper"
+	style="position: fixed;right: 10px;top: 50px;background: #eee;
+		width: 50px;height: 50px;z-index:99999;border:1px solid #999;
+	"
+>
+	<p class="btn-ctrl" style="background: greenyellow;">play</p>
+	<p id="radio-text"></p>
+	<audio id="radio-player" src="" preload="auto" ></audio>
+</div>`;
+    const win = document.createElement('div');
+    win.innerHTML = wintip;
+    document.getElementsByTagName('body')[0].append(win);
+    const show = (e) => {
+        e = e.toString();
+        document.getElementById(tipid).textContent = e;
+    };
+    const srcArr = [
+        '//rtmpcnr001.cnr.cn/live/zgzs/playlist.m3u8',
+        '//lhttp.qingting.fm/live/386/64k.mp3'
+    ];
+    let isOn = false;
+    const wrapper = document.getElementById("radio-wrapper");
+    const audio = document.getElementById('radio-player');
+    const btn = wrapper.querySelector(".btn-ctrl");
+    audio.src = srcArr[0];
+    audio.addEventListener('error', function (e) {
+        audio.src = srcArr.shift();
+        if (isOn) {
+            audio.play()
+        }
+
+    });
+    btn.addEventListener('click', function () {
+        isOn = true;
+        if (audio.paused === true) {
+            audio.play()
+        } else {
+            audio.pause()
+            btn.textContent = 'play';
+        }
+        setTimeout(function () {
+            if (!audio.paused) {
+                btn.textContent = 'stop';
+            }
+        }, 1000)
+    });
+})();
