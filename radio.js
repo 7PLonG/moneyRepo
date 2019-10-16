a75906fa6de82b3731a8fc1dcf50988a4fa03b60
孙杨博 <1412sybbob@163.com>
2019-10-16 16:57:40 +0800

    Update radio.js
    将报错逻辑限定在srcarr length上，
    并将插件范围限定在https://fanyi.baidu.com/*；
    防止加载过多后被chrome限制。

--- radio.js
+++ radio.js
@@ -4,60 +4,63 @@
 // @version      0.1
 // @description  直引到中国之声
 // @author       syb | 7P_LonG
-// @include *
+// @include https://fanyi.baidu.com/*
 // @grant        none
 // ==/UserScript==
 
 
-(function () {
-    'use strict';
 
-    const tipid = 'radio-text';//提示框id
-    const wintip = `<div id="radio-wrapper"
+(function() {
+    'use strict';
+    //仅挂载到百度翻译下，因为如所有页面均加载插件，请求超过一定次数时，貌似chrome会做限制。
+const tipid = 'radio-text';//提示框id
+const wintip = `<div id="radio-wrapper"
 	style="position: fixed;right: 10px;top: 50px;background: #eee;
 		width: 50px;height: 50px;z-index:99999;border:1px solid #999;">
 	<p class="btn-ctrl" style="background: greenyellow;">play</p>
 	<p id="radio-text"></p>
 	<audio id="radio-player" src="" preload="auto" ></audio>
 </div>`;
-    const win = document.createElement('div');
-    win.innerHTML = wintip;
-    document.getElementsByTagName('body')[0].append(win);
-    const show = (e) => {
-        e = e.toString();
-        document.getElementById(tipid).textContent = e;
-    };
-    const srcArr = [
-        '//rtmpcnr001.cnr.cn/live/zgzs/playlist.m3u8',
-        '//lhttp.qingting.fm/live/386/64k.mp3'
-    ];
-    let isOn = false;
-    const wrapper = document.getElementById("radio-wrapper");
-    const audio = document.getElementById('radio-player');
-    const btn = wrapper.querySelector(".btn-ctrl");
-    audio.src = srcArr[0];
-    //如官方链接报错，直接移除到下一个资源。
-    //[1]为三方备用资源，有延时
-    //支持edge,chrome,firefox
-    audio.addEventListener('error', function (e) {
-        audio.src = srcArr.shift();
-        if (isOn) {
-            audio.play()
+        const win = document.createElement('div');
+        win.innerHTML = wintip;
+        document.getElementsByTagName('body')[0].append(win);
+        const show = (e)=>{
+            e = e.toString();
+            document.getElementById(tipid).textContent= e;
+        };
+const srcArr = [
+    '//rtmpcnr001.cnr.cn/live/zgzs/playlist.m3u8',
+    '//lhttp.qingting.fm/live/386/64k.mp3'
+];
+let isOn = false;
+const wrapper = document.getElementById("radio-wrapper");
+const audio = document.getElementById('radio-player');
+const btn = wrapper.querySelector(".btn-ctrl");
+const errfn = function(e){
+    console.log(e)
+    if(srcArr.length === 0){
+         console.log('全资源失效');
+        audio.removeEventListener('error',errfn);
+         return false;
+    }
+    audio.src = srcArr.shift();
+    if(isOn){audio.play()}
+};
+audio.src = srcArr[0];
+audio.addEventListener('error',errfn);
+
+btn.addEventListener('click',function(){
+    isOn = true;
+    if(audio.paused === true){
+        audio.play()
+    }else{
+        audio.pause()
+        btn.textContent= 'play';
+    }
+    setTimeout(function(){
+        if(!audio.paused){
+            btn.textContent= 'stop';
         }
-    });
-	
-    btn.addEventListener('click', function () {
-        isOn = true;
-        if (audio.paused === true) {
-            audio.play()
-        } else {
-            audio.pause()
-            btn.textContent = 'play';
-        }
-        setTimeout(function () {
-            if (!audio.paused) {
-                btn.textContent = 'stop';
-            }
-        }, 1000)
-    });
+    },1000)
+});
 })();
